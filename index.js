3c79c14e68436416e57e5223ce96fea72dc98f05
Alan Morford <igginz@gmail.com>
2020-06-06 15:23:50 -0700

    Add index.js with header

---
+++ index.js
@@ -0,0 +1,6 @@
+/*
+Name:           moser-de-bruijn-sequence
+Description:    This function calculates moser-de-bruijn-sequence numbers
+Author:         Alan Morford
+Source & docs:  https://github.com/alanmorford/moser-de-bruijn-sequence
+*/
