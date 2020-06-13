0cbee8da8b6777a7df08476d9b2e2a892bf4f953
Alan Morford <igginz@gmail.com>
2020-06-13 14:06:33 -0700

    Bring in module and function and doTest both of these "exports"

--- test.js
+++ test.js
@@ -3,3 +3,19 @@ Name:           moser-de-bruijn-sequence - test.js
 */
 
 'use strict';
+
+const mdbModule = require ('./');
+const moserDeBruijnSequence = mdbModule.moserDeBruijnSequence;
+const dotest = require ('dotest');
+
+// module basics
+dotest.add ('Module', test => {
+
+  test ()
+    .isObject ('fail', 'exports', mdbModule)
+    .isFunction ('fail', 'moserDeBruijnSequence', moserDeBruijnSequence)
+    .done ();
+});
+
+// Start the tests
+dotest.run ();
